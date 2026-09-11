5aa142b6729fa492a499d4aea728a329849f530c
yang <7thwesk@gmail.com>
2026-09-11 22:03:08 +0800

    1

--- hello.go
+++ hello.go
@@ -2,9 +2,20 @@ package main
 import "fmt"
 import "time"
 
+const (
+	BEIJING = 110
+	SHANGHAI = 310
+)
+
+func fn(city1 string, city2 string) (int, int) {
+	if city1 == "Beijing" && city2 == "Shanghai" {
+		return BEIJING, SHANGHAI
+	}
+	return 0, 0
+}
 
 func main(){
 	fmt.Println("Hello, Go!")
 	time.Sleep(1 * time.Second)
-}
+	fmt.Println(fn("Beijing", "Shanghai"))
 }
