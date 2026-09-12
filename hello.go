4e5fb21aa569bfa12594de7a0eb4b8ffeb206be8
yang <7thwesk@gmail.com>
2026-09-12 16:15:50 +0800

    1

--- hello.go
+++ hello.go
@@ -1,21 +1,24 @@
 package main
 import "fmt"
-import "time"
+// import "time"
+import "golang_tutorial/lib1"
 
 const (
 	BEIJING = 110
 	SHANGHAI = 310
 )
 
-func fn(city1 string, city2 string) (int, int) {
-	if city1 == "Beijing" && city2 == "Shanghai" {
-		return BEIJING, SHANGHAI
-	}
-	return 0, 0
-}
+// func fn(city1 string, city2 string) (int, int) {
+// 	if city1 == "Beijing" && city2 == "Shanghai" {
+// 		return BEIJING, SHANGHAI
+// 	}
+// 	return 0, 0
+// }
 
 func main(){
 	fmt.Println("Hello, Go!")
-	time.Sleep(1 * time.Second)
-	fmt.Println(fn("Beijing", "Shanghai"))
+	// time.Sleep(1 * time.Second)
+	// fmt.Println(fn("Beijing", "Shanghai"))
+	lib1.Lib1Func()
+
 }
---
+++ lib1/lib1.go
@@ -0,0 +1,11 @@
+package lib1
+
+import "fmt"
+
+func Lib1Func(){
+	fmt.Println("lib1 func")
+}
+
+func init() {
+	fmt.Println("lib1 init")
+}
