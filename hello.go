5dd4bba44761b6a700335e27cc8b5caeb8be96ae
yang <7thwesk@gmail.com>
2026-09-13 16:03:28 +0800

    1

--- hello.go
+++ hello.go
@@ -1,7 +1,7 @@
 package main
 import "fmt"
 // import "time"
-import "golang_tutorial/lib1"
+// import "golang_tutorial/lib1"
 
 const (
 	BEIJING = 110
@@ -16,9 +16,30 @@ const (
 // }
 
 func main(){
-	fmt.Println("Hello, Go!")
+	c := make(chan int, 3)
+
+	go func(){
+		for i:=0; i<5; i++ {
+			c <- i
+			fmt.Println("send data:", i)
+			fmt.Println("channel length:", len(c))
+			fmt.Println("channel capacity:", cap(c))
+		}
+		close(c)
+	}()
+
+	// fmt.Println("Hello, Go!")
 	// time.Sleep(1 * time.Second)
 	// fmt.Println(fn("Beijing", "Shanghai"))
-	lib1.Lib1Func()
+	// lib1.Lib1Func()
+	
+	for{
+		if data, ok := <-c; ok {
+			fmt.Println(data)
+		}else{
+			break
+		}
+	}
+
 
 }
